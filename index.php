eb5b53880d288f4b0f62fdc607b083b242c68cef
ziza2005 <isaqueborgestaioque@gmail.com>
2023-07-05 10:25:56 -0300

    teste

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 
-<head>
+<head> 
   <!-- Meta tags Obrigatórias -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
